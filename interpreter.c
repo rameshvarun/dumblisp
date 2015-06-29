8296237d680a127aeea85549cda4f20b17fa1626
Varun Ramesh <varunramesh@fb.com>
2015-06-29 14:36:47 -0700

    Basic lexer working

--- interpreter.c
+++ interpreter.c
@@ -1,2 +1,24 @@
+#include "lexer.h"
+
 int main(int argc, char** argv) {
+  if(argc == 2) {
+    lexing_context ctx;
+    create_lexing_context(&ctx, argv[1]);
+
+    while(true) {
+      token_t tok;
+      get_next_token(&ctx, &tok);
+
+      switch(tok.type) {
+      case STRING:
+      case SYMBOL:
+	printf("%s at %s:%d - %s\n", get_token_type_name(tok.type), ctx.filename, tok.lineno, tok.data.string_value);
+	break;
+      default:
+	printf("%s at %s:%d\n", get_token_type_name(tok.type), ctx.filename, tok.lineno);
+      }
+      
+      if(tok.type == EOF_TOKEN) break;
+    }
+  }
 }
---
+++ lexer.h
@@ -0,0 +1,44 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#define MAX_LITERAL_SIZE 4096
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <strings.h>
+
+typedef enum {
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  STRING,
+  SYMBOL,
+  QUOTE,
+  EOF_TOKEN
+} TOKEN_TYPE;
+
+typedef struct {
+  TOKEN_TYPE type;
+  int lineno;
+
+  union {
+    const char* string_value;
+    int int_value;
+  } data;
+} token_t;
+
+typedef struct {
+  FILE* file;
+  int lineno;
+  const char* filename;
+  int errors;
+} lexing_context;
+
+void create_lexing_context(lexing_context* ctx, const char* filename);
+void get_next_token(lexing_context* ctx, token_t* token);
+const char* get_token_type_name(TOKEN_TYPE type);
+bool is_whitespace(char c);
+bool is_digit(char c);
+bool is_letter(char c);
+bool is_alphanumeric(char c);
+
+#endif

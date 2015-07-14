bbaceb7478cbfdd9f56f465771ed25b3ae9a6adb
Varun Ramesh <rameshvarun@users.noreply.github.com>
2015-07-14 16:30:11 -0700

    Rearrange builtins.h

--- src/builtins.h
+++ src/builtins.h
@@ -4,6 +4,7 @@
 #include "scope.h"
 #include "expression.h"
 
+// IO functions
 struct expr *builtin_print(struct scope *scope, struct expr *arguments);
 
 // Control flow
@@ -24,12 +25,13 @@ struct expr *builtin_mult(struct scope *scope, struct expr *arguments);
 struct expr *builtin_sub(struct scope *scope, struct expr *arguments);
 struct expr *builtin_div(struct scope *scope, struct expr *arguments);
 
+// Set variables / create new scopes
 struct expr *builtin_let(struct scope *scope, struct expr *arguments);
 struct expr *builtin_plet(struct scope *scope, struct expr *arguments);
+struct expr *builtin_set(struct scope *scope, struct expr *arguments);
 
+// Function creation
 struct expr *builtin_lambda(struct scope *scope, struct expr *arguments);
 struct expr *builtin_defun(struct scope *scope, struct expr *arguments);
-
-struct expr *builtin_set(struct scope *scope, struct expr *arguments);
 
 #endif

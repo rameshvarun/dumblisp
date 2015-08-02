0c249a125b7f504a64d9d44d8545708be1b7e02f
Varun Ramesh <varunramesh@fb.com>
2015-08-02 14:15:34 -0700

    Added some library functions.

--- src/lib.lisp
+++ src/lib.lisp
@@ -1,3 +1,25 @@
-;; Increment Macro
+;; Increment and Decrement Macros
 (defmacro ++ (x)
   (list 'set x (list '+ x 1)))
+(defmacro -- (x)
+  (list 'set x (list '- x 1)))
+
+;; Arithmatic + Assignment
+(defmacro += (x y)
+  (list 'set x (list '+ x y)))
+(defmacro -= (x y)
+  (list 'set x (list '- x y)))
+(defmacro *= (x y)
+  (list 'set x (list '* x y)))
+(defmacro /= (x y)
+  (list 'set x (list '/ x y)))
+
+;; >= and <=
+(defmacro <= (x y)
+  (list 'or (list '< x y) (list '= x y)))
+(defmacro >= (x y)
+  (list 'or (list '> x y) (list '= x y)))
+
+;; Negation
+(defmacro ~ (x)
+	(list '- 0 x))
